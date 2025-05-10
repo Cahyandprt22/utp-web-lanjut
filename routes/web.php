4884afb316607d368e0da7d993198dede6927232
Cahyandprt22 <nailacahya580@gmail.com>
2025-05-10 15:48:03 +0700

    part 5

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\TeacherController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,5 +19,6 @@ Route::get('/', function () {
     return view('layout');
 });
 
-
 Route::resource('/students', StudentController::class);
+Route::resource('/teachers', TeacherController::class);
+
